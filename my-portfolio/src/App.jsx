092244ab3842707a7780b52d43edd7f025af448b
Ayanara <aya.nathane@gmail.com>
2023-10-04 11:17:05 -0300

    Configurações inicinais finalizadas e corrigidas

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from "react-router-dom";
+
 import { About, Contact, Experience, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
 
 const App = () => {
