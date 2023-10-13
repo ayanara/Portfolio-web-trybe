5dd651bbb8f76542c385ae49f90aa5549973328e
Ayanara <aya.nathane@gmail.com>
2023-10-13 10:51:36 -0300

    Melhora a frase do protfolio

--- my-portfolio/src/components/Navbar.jsx
+++ my-portfolio/src/components/Navbar.jsx
@@ -32,7 +32,7 @@ const Navbar = () => {
       className={`${
         styles.paddingX
       } w-full flex items-center py-5 fixed top-0 z-20 ${
-        scrolled ? "bg-primary" : "bg-transparent"
+        scrolled ? "bg-blue-950" : "bg-transparent"
       }`}
     >
       <div className='w-full flex justify-between items-center max-w-7xl mx-auto '>
